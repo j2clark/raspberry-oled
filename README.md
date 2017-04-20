2c80c2957cdddae00a8d4f70aff1bcb59d5764b1
jclark <jclark@shutterfly.com>
2017-04-20 16:51:14 -0700

    Adding notes and an Adafruit library example

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # raspberry-oled
+
+SSD_1306 OLED on Raspberry Pi
+
+Adafruit examples found here:
+https://learn.adafruit.com/ssd1306-oled-displays-with-raspberry-pi-and-beaglebone-black?view=all
+
+Luma library 
+Nice features
+Cannot install on windows - seems to have been writen explicitly for pi:
+https://luma-oled.readthedocs.io/en/latest/index.html
+
+
+IDE/Raspberry Pi Config found here
+http://gowrishankarnath.com/remote-programming-raspberry-pi-using-pycharm/
---
+++ src/main/python/adafruit-oled.py
@@ -0,0 +1,92 @@
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+import PIL.Image as Image
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+# Raspberry Pi pin configuration:
+RST = 24
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+#disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Alternatively you can specify an explicit I2C bus number, for example
+# with the 128x32 display you would use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+
+# 128x32 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# 128x64 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Alternatively you can specify a software SPI implementation by providing
+# digital GPIO pin numbers for all the required display pins.  For example
+# on a Raspberry Pi with the 128x32 display you might use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = 2
+shape_width = 20
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = padding
+# Draw an ellipse.
+draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
+x += shape_width+padding
+# Draw a rectangle.
+draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
+x += shape_width+padding
+# Draw a triangle.
+draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
+x += shape_width+padding
+# Draw an X.
+draw.line((x, bottom, x+shape_width, top), fill=255)
+draw.line((x, top, x+shape_width, bottom), fill=255)
+x += shape_width+padding
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+# Write two lines of text.
+draw.text((x, top), 'Hello', font=font, fill=255)
+draw.text((x, top + 20), 'World!', font=font, fill=255)
+
+# Display image.
+disp.image(image)
+disp.display()
